e3c6d87951c3e05002ab4be8c07c0922090778bd
tomi946 <totoloreto73@gmail.com>
2026-04-26 20:28:38 -0300

    config de firebase actualizada, con los agregados de cambio de nro de wsp

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 // Tus datos de Firebase (Testing propio)
 const firebaseConfig = {
+<<<<<<< Updated upstream
   apiKey: "AIzaSyCqW-YicrdLtECArLfnSzItzursk7tjjyM",
   authDomain: "reservacanchas-a383c.firebaseapp.com",
   projectId: "reservacanchas-a383c",
@@ -11,10 +13,29 @@ const firebaseConfig = {
   messagingSenderId: "453571768825",
   appId: "1:453571768825:web:73560a66fc00fecf0b4d28",
   measurementId: "G-PTRSB2TW5S"
+=======
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+>>>>>>> Stashed changes
 };
 
+// Inicializamos la App
 const app = initializeApp(firebaseConfig);
 
+<<<<<<< Updated upstream
 // Exportamos lo que el resto de la app necesita
-export const db = getFirestore(app);
+=======
+// Inicializamos analitics
+export const analytics = getAnalytics(app);
+
+// Exportamos las herramientas que usamos en el proyecto
 export const auth = getAuth(app);
+>>>>>>> Stashed changes
+export const db = getFirestore(app);
+
+export default app;
